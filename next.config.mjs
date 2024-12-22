2c6c42c516375ce46bdf77359502f9d4a95835d4
Rajveer <rallrajveer@gmail.com>
2024-12-22 21:48:48 +0530

    netfliy related code added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: 'export',
+   experimental: {
+    appDir: true, // if using the app directory
+  },
    basePath: '/static-landingpage', // Replace with your GitHub repository name
     eslint: {
         // Warning: This allows production builds to successfully complete even if
