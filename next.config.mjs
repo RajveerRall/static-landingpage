ed4c35e67fb5198cb6484d41a3c631ee5b883341
Rajveer <rallrajveer@gmail.com>
2024-12-22 20:39:37 +0530

    generated a new out folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: 'export',
+   basePath: '/static-landingpage', // Replace with your GitHub repository name
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
