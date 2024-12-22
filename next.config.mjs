a51f0b5d99bd94d9c1cdd171aa830afbdb5ee28f
Rajveer <rallrajveer@gmail.com>
2024-12-22 21:54:03 +0530

    removed the basepath

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
    experimental: {
     appDir: true, // if using the app directory
   },
-   basePath: '/static-landingpage', // Replace with your GitHub repository name
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
