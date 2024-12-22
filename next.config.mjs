61cf6c53cc595301add152d5efdb624bbb42cab5
Rajveer <rallrajveer@gmail.com>
2024-12-22 23:55:25 +0530

    created public folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: 'export',
-   experimental: {
-    appDir: true, // if using the app directory
-  },
+  //  experimental: {
+  //   appDir: true, // if using the app directory
+  // },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
