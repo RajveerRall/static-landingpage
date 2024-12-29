8787cec7f68dda9de091214def413abab5edb280
Rajveer <rallrajveer@gmail.com>
2024-12-29 22:06:56 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,11 @@
 const config = {
   reactStrictMode: true,
   output: 'export',
-  experimental: {
-    appDir: true,
-  },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 };
 
 export default config;
